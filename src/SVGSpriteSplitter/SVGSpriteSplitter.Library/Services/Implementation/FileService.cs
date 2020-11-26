4c9816a49ac74ee907dd6760c0377e4521e1d194
Radoslav Yordanov <yordanov1234@gmail.com>
2020-11-26 09:34:38 +0200

    Added GetSVGSpriteFileContent method at FileService.cs

--- src/SVGSpriteSplitter/SVGSpriteSplitter.Library/Services/Implementation/FileService.cs
+++ src/SVGSpriteSplitter/SVGSpriteSplitter.Library/Services/Implementation/FileService.cs
@@ -1,6 +1,23 @@
-﻿namespace SVGSpriteSplitter.Library.Services.Implementation
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace SVGSpriteSplitter.Library.Services.Implementation
 {
     internal class FileService
     {
+        internal string GetSVGSpriteFileContent(string spriteFileFullPath)
+        {
+            string content = null;
+
+            if (File.Exists(spriteFileFullPath))
+            {
+                using (var stream = new StreamReader($"{spriteFileFullPath}"))
+                {
+                    content = stream.ReadToEnd();
+                };
+            }
+
+            return content;
+        }
     }
 }
