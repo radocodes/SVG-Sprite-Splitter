d678a8ae993f0e1c8f73e4d20ff780cb4a8f86cd
Radoslav Yordanov <yordanov1234@gmail.com>
2020-11-26 21:06:48 +0200

    Rename Program.cs to Startup.cs

---
+++ src/SVGSpriteSplitter/ConsoleApp/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile;
+            string destinationFolder;
+            bool resultIsSuccessfull = false;
+
+            var inputOutput = new IO();
+            var splitter = new SVGSpriteSplitter.Library.SVGSpriteSplitter();
+
+            inputOutput.ConsolePrintWelcomeMesssage();
+
+            inputOutput.ConsolePrintSourceInputInstruction();
+            sourceFile = inputOutput.ConsoleReadParameter();
+            inputOutput.ConsolePrintFreeLine();
+
+            inputOutput.ConsolePrintDestinationInputInstruction();
+            destinationFolder = inputOutput.ConsoleReadParameter();
+            inputOutput.ConsolePrintFreeLine();
+
+            if (!string.IsNullOrEmpty(sourceFile) && !string.IsNullOrEmpty(destinationFolder))
+            {
+                inputOutput.ConsolePrintLoadingMessage();
+                resultIsSuccessfull = splitter.Split(sourceFile, destinationFolder);
+            }
+
+            inputOutput.ConsolePrintSmallSeparationLine();
+            inputOutput.ConsolePrintSmallSeparationLine();
+
+            inputOutput.ConsolePrintEndTaskMessage();
+
+            if (resultIsSuccessfull)
+            {
+                inputOutput.ConsolePrintSuccessfullResultMessage();
+            }
+            else
+            {
+                inputOutput.ConsolePrintUnSuccessfullResultMessage();
+            }
+
+            inputOutput.ConsolePrintQuitAppInstruction();
+            inputOutput.ConsoleReadParameter();
+        }
+    }
+}
